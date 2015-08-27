8394e362a73093696030462256c3a922148d835d
ZPJCPP <zpj090724@163.com>
2015-08-28 01:50:23 +0800

    test

--- es6/1-let&const/source.js
+++ es6/1-let&const/source.js
@@ -1,12 +1,22 @@
 import nodeunit from 'nodeunit'; 
 var testSomething = function(test){
-    test.expect(1);
+    test.expect(7);
     test.ok(true, "this assertion should pass");
+    test.equal(1,'1', "this assertion should equal");
+    test.notEqual(1,2, "this assertion should notEqual");
+    test.deepEqual(1,1, "this assertion should deepEqual");
+    test.notDeepEqual(1,2, "this assertion should notDeepEqual");
+    test.strictEqual(1,1, "this assertion should strictEqual");
+    test.notStrictEqual(1,2, "this assertion should notStrictEqual");
     test.done();
 };
+var testLetVar = function(test){
+    var a = [];
+    for (let i = 0; i < 10; i++) {
+        a[i] =  () =>{return i};
+    }
+    test.equal(a[6](),6, "a[6]() 应该等于6");
+    test.done();
+};
+exports.obj={testLetVar};
 
-var testSomethingElse = function(test){
-    test.ok(false, "this assertion should fail");
-    test.done();
-};
-exports.obj={testSomething,testSomethingElse};
--- es6/run.js
+++ es6/run.js
@@ -1,3 +1,3 @@
 import source from './1-let&const/source.js';
 import nodeunit from 'nodeunit'; 
-nodeunit.testrunner.run({"source":source})
+nodeunit.reporters.default.run({"source":source})
